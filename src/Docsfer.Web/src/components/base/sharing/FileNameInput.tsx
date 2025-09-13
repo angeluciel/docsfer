8996d0f05d2ef4c5ccd237645985de12b090d2f0
Henrique Maia Cardosa <henrique.maiacardosa@gmail.com>
2025-09-13 15:57:04 -0300

    Label color and input shape/color

--- src/Docsfer.Web/src/components/base/sharing/FileNameInput.tsx
+++ src/Docsfer.Web/src/components/base/sharing/FileNameInput.tsx
@@ -3,14 +3,19 @@ import SwitchFileButton from "./SwitchFileButton";
 export default function FileNameInput() {
 return(
     <div className="flex flex-col items-center justify-center">
-        <label htmlFor="fileName">
+      <div className="w-full max-w-md space-y-3">
+        <label htmlFor="fileName"
+        className="block tex-[20px] font-semibold text-[#243D5A]">
             Nome do arquivo
         </label>
-      <input type="text" id="fileName" className="" />
+      <input type="text" 
+      id="fileName" 
+      className="w-half' rounded-md border border-zinc-300" />
 
 
     {/*Botão do Switch*/}
    <SwitchFileButton/>
+      </div>
     </div>
   )  
 }
